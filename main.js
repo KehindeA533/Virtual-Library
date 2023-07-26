b35f3e491aedcb6d9b7269d50f1efb5dc787d55c
Kehinde. A <whenkehindewalks@gmail.com>
2023-07-26 00:56:43 -0400

    updated Media

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 // Import classes here to console.log and debug
+const Media = require("./classes/Media");
+
+const book = new Media("The Catcher in the Rye", 1951, "Fiction");
+console.log(Media.totalMediaCount); // 1
+const music = new Media("Abbey Road", 1969, "Rock");
+console.log(Media.totalMediaCount); // 2
+console.log(music.summary());
