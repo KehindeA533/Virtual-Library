7299d96393de661441bb2637b65b8722247ddb3a
Kehinde. A <whenkehindewalks@gmail.com>
2023-07-26 16:19:32 -0400

    updated Music

--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,32 @@
 // import the Media class:
 const Media = require("./Media");
-// create your Music class:
 
+// create your Music class:
+class Music extends Media {
+  static shortestAlbum(Music) {
+    let shortest = Music[0].length;
+    for (let i = 0; i < Music.length; i++) {
+      if (Music[i].length <= shortest) {
+        shortest = Music[i].length;
+      }
+    }
+
+    for (let i = 0; i < Music.length; i++) {
+      if (Music[i].length === shortest) {
+        return Music[i];
+      }
+    }
+  }
+
+  constructor(title, year, genre, artist, length) {
+    super(title, year, genre);
+    this.artist = artist;
+    this.length = length;
+  }
+
+  summary() {
+    return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length}`;
+  }
+}
 // don't change below
 module.exports = Music;
--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const Media = require("./classes/Media");
 const Movie = require("./classes/Movie");
 const Book = require("./classes/Book");
-// const Music = require("./classes/Music");
+const Music = require("./classes/Music");
 
 //
 // Media
@@ -39,27 +39,40 @@ const Book = require("./classes/Book");
 //
 // Book
 //
-const book1 = new Book(
-  "To Kill a Mockingbird",
-  1960,
-  "Fiction",
-  "Harper Lee",
-  281,
-  4.4
-);
-console.log(Media.totalMediaCount); // 1
-const book2 = new Book(
-  "The Bluest Eye",
-  1970,
-  "Fiction",
-  "Toni Morrison",
-  206,
-  4.6
-);
-console.log(Media.totalMediaCount); // 2
-console.log(book1.summary()); // "Title: To Kill a Mockingbird, Author: Harper Lee, Year: 1960, Page Count: 281, Genre: Fiction, Rating: 4.4"
-console.log(Book.highestRating([book1, book2])); // Returns book2
+// const book1 = new Book(
+//   "To Kill a Mockingbird",
+//   1960,
+//   "Fiction",
+//   "Harper Lee",
+//   281,
+//   4.4
+// );
+// console.log(Media.totalMediaCount); // 1
+// const book2 = new Book(
+//   "The Bluest Eye",
+//   1970,
+//   "Fiction",
+//   "Toni Morrison",
+//   206,
+//   4.6
+// );
+// console.log(Media.totalMediaCount); // 2
+// console.log(book1.summary()); // "Title: To Kill a Mockingbird, Author: Harper Lee, Year: 1960, Page Count: 281, Genre: Fiction, Rating: 4.4"
+// console.log(Book.highestRating([book1, book2])); // Returns book2
 
 //
 // Movie
 //
+
+const music1 = new Music("Lemonade", 2016, "R&B", "Beyonce", "Lemonade", 3949);
+const music2 = new Music(
+  "Renaissance",
+  2022,
+  "R&B",
+  "Beyonce",
+  "Beyonce",
+  3734
+);
+console.log(Media.totalMediaCount); // 2
+console.log(music2.summary()); // "Title: Renaissance, Artist: Beyonce, Year: 2022, Genre: R&B, Length: 3734 seconds"
+console.log(Music.shortestAlbum([music1, music2])); // Returns music2
